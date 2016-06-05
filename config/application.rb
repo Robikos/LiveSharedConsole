b2e7bf12283d0a5ce4e2e225792f2b0a312e9836
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-06-05 23:38:06 +0200

    Add necessary changes while bumping rails

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Reactapp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    ActiveSupport.halt_callback_chains_on_return_false = false
   end
 end
