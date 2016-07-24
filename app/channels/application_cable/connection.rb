e2334f932fd45d1d30a77045b8bf8724060db0c7
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-07-24 16:47:51 +0200

    Fix setting current user for web socket connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,19 +3,10 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_current_user
+      self.current_user = find_verified_user
     end
 
     protected
-
-    def find_current_user
-      user = User.find_by(id: cookies.signed[:user_id])
-      if user && cookies.signed[:expires_at] > Time.now
-        user
-      else
-        reject_unauthorized_connection
-      end
-    end
 
     def find_verified_user
       verified_user = User.find_by(id: cookies.signed['user.id'])
