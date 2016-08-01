1ad0a0004129423f4fdf381475b4d1bb171cd541
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-08-01 23:08:22 +0200

    Apply flow services in subscribe, unsubscribe and receive actions

--- app/channels/console_channel.rb
+++ app/channels/console_channel.rb
@@ -1,11 +1,23 @@
 class ConsoleChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "console_stream"
+    RoomCreator.new(room_id, current_user).call
+    stream_from "console_stream_#{room_id}"
+    UserJoiner.new(room_id, current_user).call
   end
 
   def unsubscribed
+    UserLeaver.new(room_id, current_user).call
+    RoomDestroyer.new(room_id, current_user).call
   end
 
   def received(data)
+    result = ActionPerformer.new(room_id, data).call
+    OutputUpdater.new(result).call
+  end
+
+  private
+
+  def room_id
+    params[:id]
   end
 end
