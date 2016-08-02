ec83d63a4729c18b151421fc06ab3cbd5978f2a0
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-08-02 02:56:24 +0200

    Complete UserLeaver service

--- app/services/user_leaver.rb
+++ app/services/user_leaver.rb
@@ -5,9 +5,28 @@ class UserLeaver
   end
 
   def call
+    send_goodbye_message
+    unassign_user_from_room
   end
 
   private
+
+  def send_goodbye_message
+    ActionCable.server.broadcast(
+      room_stream_name,
+      {
+        message: "#{current_user.email} left this room"
+      }
+    )
+  end
+
+  def unassign_user_from_room
+    current_user.update!(room: nil)
+  end
+
+  def room_stream_name
+    "console_stream_#{room_id}"
+  end
 
   attr_reader :room_id, :current_user
 end
