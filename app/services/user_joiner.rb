19570fcf8171e2a638f199895844ac7cca4eba2c
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-08-02 02:55:51 +0200

    Complete UserJoiner service

--- app/services/user_joiner.rb
+++ app/services/user_joiner.rb
@@ -5,9 +5,34 @@ class UserJoiner
   end
 
   def call
+    send_welcome_message
+    assign_user_to_room
   end
 
   private
+
+  def send_welcome_message
+    ActionCable.server.broadcast(
+      room_stream_name,
+      {
+        message: "#{current_user.email} joined this room"
+      }
+    )
+  end
+
+  def assign_user_to_room
+    return if current_user.room == room
+
+    current_user.update!(room: room)
+  end
+
+  def room_stream_name
+    "console_stream_#{room_id}"
+  end
+
+  def room
+    @room ||= Room.find(room_id)
+  end
 
   attr_reader :room_id, :current_user
 end
