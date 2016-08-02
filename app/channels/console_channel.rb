f208c697e09333b5ec461739f462d9f616ae139f
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-08-02 02:57:28 +0200

    Improve console channel
    - return if it’s not triggered with room_id
    - change order of subscribed functions

--- app/channels/console_channel.rb
+++ app/channels/console_channel.rb
@@ -1,18 +1,21 @@
 class ConsoleChannel < ApplicationCable::Channel
   def subscribed
+    return if room_id.blank?
     RoomCreator.new(room_id, current_user).call
-    stream_from "console_stream_#{room_id}"
     UserJoiner.new(room_id, current_user).call
+    stream_from "console_stream_#{room_id}"
   end
 
   def unsubscribed
+    return if room_id.blank?
     UserLeaver.new(room_id, current_user).call
     RoomDestroyer.new(room_id, current_user).call
   end
 
-  def received(data)
+  def receive(data)
+    return if room_id.blank?
     result = ActionPerformer.new(room_id, data).call
-    OutputUpdater.new(result).call
+    OutputUpdater.new(room_id, result).call
   end
 
   private
