30114f26ddcc8db7f7cd80b007d00b6cbb19a55d
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-08-02 02:56:44 +0200

    Complete OutputUpdater service

--- app/services/output_updater.rb
+++ app/services/output_updater.rb
@@ -1,12 +1,23 @@
 class OutputUpdater
-  def initialize(result)
+  def initialize(room_id, result)
+    @room_id = room_id
     @result = result
   end
 
   def call
+    ActionCable.server.broadcast(
+      room_stream_name,
+      {
+        message: "#{result}"
+      }
+    )
   end
 
   private
 
-  attr_reader :result
+  def room_stream_name
+    "console_stream_#{room_id}"
+  end
+
+  attr_reader :result, :room_id
 end
