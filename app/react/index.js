f7652fdb3ee520b0c85ed12f5ea47a6cb2ec796c
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-10-19 22:36:27 +0200

    Add redux

--- app/react/index.js
+++ app/react/index.js
@@ -1,27 +1,7 @@
-import RWR from 'react-webpack-rails';
+import RWR, { integrationsManager } from 'react-webpack-rails';
+import RWRRedux from 'rwr-redux';
+
 RWR.run();
 
-import App from './containers/app';
-RWR.registerComponent('App', App);
-
-
-
-
-//lol
-
-// import RWR, { integrationsManager } from 'react-webpack-rails';
-// import RWRRedux from 'rwr-redux';
-
-// RWR.run();
-
-// integrationsManager.register('redux-store', RWRRedux.storeIntegrationWrapper);
-// integrationsManager.register('redux-container', RWRRedux.containerIntegrationWrapper);
-
-// import CounterStore from './store';
-// RWRRedux.registerStore('CounterStore', CounterStore);
-
-// import Navbar from './containers/Navbar';
-// RWRRedux.registerContainer('Navbar', Navbar);
-
-// import CounterApp from './containers/App';
-// RWRRedux.registerContainer('CounterApp', CounterApp);
+integrationsManager.register('redux-store', RWRRedux.storeIntegrationWrapper);
+integrationsManager.register('redux-container', RWRRedux.containerIntegrationWrapper);
