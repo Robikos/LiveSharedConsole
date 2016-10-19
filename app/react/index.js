d0d0d7b9a0fb3145a0a08a47ec05d245052d8c9d
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-10-19 22:59:57 +0200

    Use store and container in app

--- app/react/index.js
+++ app/react/index.js
@@ -1,7 +1,11 @@
 import RWR, { integrationsManager } from 'react-webpack-rails';
 import RWRRedux from 'rwr-redux';
 
-RWR.run();
-
 integrationsManager.register('redux-store', RWRRedux.storeIntegrationWrapper);
 integrationsManager.register('redux-container', RWRRedux.containerIntegrationWrapper);
+
+import Store from './store/store';
+RWRRedux.registerStore('Store', Store);
+
+import App from './containers/app';
+RWRRedux.registerContainer('App', App);
