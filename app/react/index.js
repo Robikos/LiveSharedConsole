19ce360a55957e6f7f296359fe9b6ae2cb4aacef
Robert Kostrzewski <robert.kostrzewski@netguru.pl>
2016-10-19 23:23:40 +0200

    Remove RWR-Redux initializers

--- app/react/index.js
+++ app/react/index.js
@@ -1,11 +1,5 @@
-import RWR, { integrationsManager } from 'react-webpack-rails';
-import RWRRedux from 'rwr-redux';
-
-integrationsManager.register('redux-store', RWRRedux.storeIntegrationWrapper);
-integrationsManager.register('redux-container', RWRRedux.containerIntegrationWrapper);
-
-import Store from './store/store';
-RWRRedux.registerStore('Store', Store);
+import RWR from 'react-webpack-rails';
+RWR.run();
 
 import App from './containers/app';
-RWRRedux.registerContainer('App', App);
+RWR.registerComponent('App', App);
